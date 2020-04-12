3e5519b2bce36f5e48d1be0a4a0b1b5cac224977
Maige12 <42675268+Maige12@users.noreply.github.com>
2020-04-12 20:02:12 +0800

    Add files via upload

---
+++ WinCon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCon : MonoBehaviour
+{
+    #region Attributes
+    public bool winConAchieved;
+    // Activates when the player has acheieved what is needed to beat the level.
+    #endregion 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            print("The winCon has been achieved");
+        }
+        else 
+        {
+            print("invalid");
+        } 
+        
+    }
+    void Start()
+    {
+    
+    winConAchieved = true;
+    //For now, the level starts with the win condition acheived
+
+
+    }
+}
