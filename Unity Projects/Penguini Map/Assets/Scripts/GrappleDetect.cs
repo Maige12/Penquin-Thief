6a84dea8f7fd3ba0e0223881f2ee46b1271f448f
VoxAndrews <VoxAndrews@users.noreply.github.com>
2020-05-18 11:10:01 +0800

    Main Unity Project - USE THIS TO DEVELOP GAME

---
+++ Unity Projects/Penguini Map/Assets/Scripts/GrappleDetect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GrappleDetect : MonoBehaviour
+{
+
+    public GameObject player;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hookable")
+        {
+            player.GetComponent<Grapple>().hooked = true;
+            player.GetComponent<Grapple>().hookedObj = other.gameObject;
+        }
+    }
+}
