254f7e0b9f43dec9652c2ce9e832e05f1303a503
VoxAndrews <VoxAndrews@users.noreply.github.com>
2020-05-27 17:19:24 +0800

    Merging Porjects/Scenes

---
+++ Unity Projects/Merging Projects/Assets/Scripts/GrappleDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PLEASE COMMENT THIS CODE
+
+public class GrappleDetect : MonoBehaviour
+{
+    public GameObject player;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hookable")
+        {
+            player.GetComponent<Grapple>().hooked = true;
+            player.GetComponent<Grapple>().hookedObj = other.gameObject;
+        }
+    }
+}
